068aa0dd4f87323a9cf3066e5f3602992a5f6066
shynnie <hung.le150049@vnuk.edu.vn>
2017-10-28 21:58:20 +0700

    Add library

--- app/src/main/java/vn/edu/vnuk/vnuk_sharing/Functional/SyllabusScreen.java
+++ app/src/main/java/vn/edu/vnuk/vnuk_sharing/Functional/SyllabusScreen.java
@@ -1,6 +1,7 @@
 package vn.edu.vnuk.vnuk_sharing.Functional;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -8,6 +9,7 @@ import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
