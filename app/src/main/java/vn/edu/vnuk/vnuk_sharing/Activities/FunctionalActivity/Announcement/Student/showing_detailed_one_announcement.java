b0f73b53cb67818160de4a1e4bbc64661d907e74
duongduchung <hung13011997@gmail.com>
2017-11-15 20:02:51 +0700

    Adding detailed deadline & announcement

---
+++ app/src/main/java/vn/edu/vnuk/vnuk_sharing/Activities/FunctionalActivity/Announcement/Student/showing_detailed_one_announcement.java
@@ -0,0 +1,15 @@
+package vn.edu.vnuk.vnuk_sharing.Activities.FunctionalActivity.Announcement.Student;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.edu.vnuk.vnuk_sharing.R;
+
+public class showing_detailed_one_announcement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showing_detailed_one_announcement);
+    }
+}
