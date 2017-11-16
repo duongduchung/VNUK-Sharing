ba121bfd9802866973379c15bafb4bb57427404c
shynnie <hung.le150049@vnuk.edu.vn>
2017-11-16 15:07:10 +0700

    Edit detail layout announcement and deadline

--- app/src/main/java/vn/edu/vnuk/vnuk_sharing/Activities/FunctionalActivity/Announcement/Student/showing_detailed_one_announcement.java
+++ app/src/main/java/vn/edu/vnuk/vnuk_sharing/Activities/FunctionalActivity/Announcement/Student/showing_detailed_one_announcement.java
@@ -2,14 +2,29 @@ package vn.edu.vnuk.vnuk_sharing.Activities.FunctionalActivity.Announcement.Stud
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import vn.edu.vnuk.vnuk_sharing.R;
 
 public class showing_detailed_one_announcement extends AppCompatActivity {
 
+    TextView tvSubject,tvContent,tvDate,tvCourse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showing_detailed_one_announcement);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Day-off Notification");
+
+        tvSubject = (TextView) findViewById(R.id.tv_subject2);
+        tvContent = (TextView) findViewById(R.id.tv_content2);
+        tvDate = (TextView) findViewById(R.id.tv_date2);
+        tvCourse =(TextView) findViewById(R.id.tv_course2);
+
+
+
     }
 }
--- app/src/main/java/vn/edu/vnuk/vnuk_sharing/Activities/FunctionalActivity/Deadline/Student/showing_detailed_one_deadline.java
+++ app/src/main/java/vn/edu/vnuk/vnuk_sharing/Activities/FunctionalActivity/Deadline/Student/showing_detailed_one_deadline.java
@@ -2,14 +2,31 @@ package vn.edu.vnuk.vnuk_sharing.Activities.FunctionalActivity.Deadline.Student;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import vn.edu.vnuk.vnuk_sharing.R;
 
 public class showing_detailed_one_deadline extends AppCompatActivity {
 
+    TextView tvDate,tvSubject,tvContent,tvCourse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showing_detailed_one_deadline);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Deadlines notification");
+
+        tvSubject = (TextView) findViewById(R.id.tv_subject2);
+        tvContent = (TextView) findViewById(R.id.tv_content2);
+        tvDate = (TextView) findViewById(R.id.tv_date2);
+        tvCourse =(TextView) findViewById(R.id.tv_course2);
+
+
+
+
     }
 }
